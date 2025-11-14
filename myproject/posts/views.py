5abde0fcd821ff876c5d792e1f13d67bf4340417
Layanjala <layanjalajayasundara@gmail.com>
2025-11-14 13:52:25 +0530

    slug

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'posts/posts_list.html', {'posts': posts})
+
+def post_page(request, slug):
+    return HttpResponse(slug)
