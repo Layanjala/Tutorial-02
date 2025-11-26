64f95be3833c1cf61d63a8d4ceaf1cf28bec8f24
Layanjala <layanjalajayasundara@gmail.com>
2025-11-26 18:56:07 +0530

    upto adding images

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     body = models.TextField()
     slug = models.SlugField(max_length=75, unique=True)
     date = models.DateTimeField(auto_now_add=True)
+    banner = models.ImageField(default='Zazzle_Nature_Love_Modern_Invitation__Front_Back_-removebg-preview.png', blank=True)
 
     def __str__(self):
         return self.title
