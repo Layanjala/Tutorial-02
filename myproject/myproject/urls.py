84df394f88d5774f7fcf0b75294b8c9398d5012f
Layanjala <layanjalajayasundara@gmail.com>
2025-11-28 22:44:53 +0530

    user registration

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('posts/', include('posts.urls')),
+    # path('users/', include('users.urls')),
+    path('register/', include('users.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)    
